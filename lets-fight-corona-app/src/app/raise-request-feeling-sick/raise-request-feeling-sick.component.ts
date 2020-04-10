199d89eaf9f2d407036e7a0389e93dff1bbf0408
nitish <nitish.nc18@gmail.com>
2020-04-10 19:20:46 +0530

    added support for raising medicine and feeling sick request

--- lets-fight-corona-app/src/app/raise-request-feeling-sick/raise-request-feeling-sick.component.ts
+++ lets-fight-corona-app/src/app/raise-request-feeling-sick/raise-request-feeling-sick.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { SendDataBackendService } from '../send-data-backend.service';
 
 @Component({
   selector: 'app-raise-request-feeling-sick',
@@ -14,15 +15,34 @@ export class RaiseRequestFeelingSickComponent implements OnInit {
     city: new FormControl(''),
     pincode: new FormControl(''),
     phoneNumber: new FormControl(''),
-    peopleCount: new FormControl(''),
+    requestCommentDetail: new FormControl(''),
   });
 
   City: any = ['Bengaluru', 'Delhi', 'Pune', 'Mumbai', 'Jaipur', 'Chennai', 'Hyderabad', 'Ahmedabad']
 
-  constructor() { }
+  constructor(private sendDataToBackendService: SendDataBackendService) { }
+
+  be_response: any;
+  error: any;
 
   onSubmit() {
-    alert(this.feelingSickRequestForm.value);
+    this.be_response = "";
+    this.error = "";
+    var request_payload = {
+        'name': this.feelingSickRequestForm.getRawValue().name,
+        'address': this.feelingSickRequestForm.getRawValue().address,
+        'city': this.feelingSickRequestForm.getRawValue().city,
+        'pin_code': this.feelingSickRequestForm.getRawValue().pincode,
+        'phone_number': this.feelingSickRequestForm.getRawValue().phoneNumber,
+        'feeling_sick_detail': this.feelingSickRequestForm.getRawValue().requestCommentDetail,
+        'feeling_sick': true,
+        'request_type': "feeling_sick"
+    };
+    this.sendDataToBackendService.sendDataToBackend(request_payload)
+    .subscribe(
+       data => console.log('success', data),
+       error => console.log('oops', error)
+    );
   }
 
   ngOnInit(): void {
--- lets-fight-corona-app/src/app/raise-request-medicines/raise-request-medicines.component.ts
+++ lets-fight-corona-app/src/app/raise-request-medicines/raise-request-medicines.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { SendDataBackendService } from '../send-data-backend.service';
 
 @Component({
   selector: 'app-raise-request-medicines',
@@ -15,14 +16,34 @@ export class RaiseRequestMedicinesComponent implements OnInit {
     pincode: new FormControl(''),
     phoneNumber: new FormControl(''),
     peopleCount: new FormControl(''),
+    requestCommentDetail: new FormControl(''),
   });
 
   City: any = ['Bengaluru', 'Delhi', 'Pune', 'Mumbai', 'Jaipur', 'Chennai', 'Hyderabad', 'Ahmedabad']
 
-  constructor() { }
+  constructor(private sendDataToBackendService: SendDataBackendService) { }
+
+  be_response: any;
+  error: any;
 
   onSubmit() {
-    alert(this.medicineRequestForm.value);
+    this.be_response = "";
+    this.error = "";
+    var request_payload = {
+        'name': this.medicineRequestForm.getRawValue().name,
+        'address': this.medicineRequestForm.getRawValue().address,
+        'city': this.medicineRequestForm.getRawValue().city,
+        'pin_code': this.medicineRequestForm.getRawValue().pincode,
+        'phone_number': this.medicineRequestForm.getRawValue().phoneNumber,
+        'no_of_people': this.medicineRequestForm.getRawValue().peopleCount,
+        'medicine_request_detail': this.medicineRequestForm.getRawValue().requestCommentDetail,
+        'request_type': "medicine"
+    };
+    this.sendDataToBackendService.sendDataToBackend(request_payload)
+    .subscribe(
+       data => console.log('success', data),
+       error => console.log('oops', error)
+    );
   }
 
   ngOnInit(): void {
